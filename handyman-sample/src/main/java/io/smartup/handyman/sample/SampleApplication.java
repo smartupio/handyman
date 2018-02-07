3feeceec41d4391bbe4008ad3a1749f1b6373851
Mate Lang <mate@smartup.io>
2018-02-07 15:02:55 +0200

    Modified stuff

--- handyman-sample/src/main/java/io/smartup/handyman/sample/SampleApplication.java
+++ handyman-sample/src/main/java/io/smartup/handyman/sample/SampleApplication.java
@@ -1,8 +1,11 @@
 package io.smartup.handyman.sample;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.netflix.zuul.ZuulFilter;
+import io.smartup.handyman.CachingMaintenanceStatusProvider;
+import io.smartup.handyman.InMemoryMaintenanceStatusService;
+import io.smartup.handyman.MaintenanceStatusManager;
+import io.smartup.handyman.MaintenanceStatusProvider;
 import io.smartup.handyman.MaintenanceZuulFilter;
 import io.smartup.handyman.s3.S3MaintenanceStatusService;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +15,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @Configuration
 @EnableZuulServer
@@ -19,19 +24,24 @@ import org.springframework.context.annotation.Configuration;
 public class SampleApplication {
 
     private final static String S3_FILE_NAME = "maintenance-mode-file.json";
-    private final static String BUCKET_NAME = "bucket";
+    private final static String BUCKET_NAME = "handyman-test";
+
+    private S3MaintenanceStatusService s3;
 
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
 
-//    @Bean
-//    public InMemoryMaintenanceStatusService service() {
-//        return new InMemoryMaintenanceStatusService();
-//    }
+    @PostConstruct
+    public void init() {
+        s3 = s3Service();
+    }
 
-    @Bean
-    public S3MaintenanceStatusService service() {
+    public InMemoryMaintenanceStatusService inMemoryService() {
+        return new InMemoryMaintenanceStatusService();
+    }
+
+    public S3MaintenanceStatusService s3Service() {
         return S3MaintenanceStatusService.builder()
                 .withAmazonS3(
                         AmazonS3ClientBuilder.standard()
@@ -42,9 +52,20 @@ public class SampleApplication {
                 .build();
     }
 
+
+    @Bean
+    public MaintenanceStatusProvider provider() {
+        return new CachingMaintenanceStatusProvider(s3);
+    }
+
+    @Bean
+    public MaintenanceStatusManager manager(){
+        return s3;
+    }
+
     @Bean
     public ZuulFilter maintenanceFilter() {
-        return new MaintenanceZuulFilter(service());
+        return new MaintenanceZuulFilter(provider());
     }
 
 }
