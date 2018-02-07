ebc5e37673f6b4ddf86d66325a3a32308e7d3c48
Mate Lang <mate@smartup.io>
2018-02-07 14:32:57 +0200

    Added another bean for S3Service

--- handyman-sample/src/main/java/io/smartup/handyman/sample/SampleApplication.java
+++ handyman-sample/src/main/java/io/smartup/handyman/sample/SampleApplication.java
@@ -1,8 +1,10 @@
 package io.smartup.handyman.sample;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.netflix.zuul.ZuulFilter;
-import io.smartup.handyman.InMemoryMaintenanceService;
 import io.smartup.handyman.MaintenanceZuulFilter;
+import io.smartup.handyman.s3.S3MaintenanceStatusService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -16,18 +18,33 @@ import org.springframework.context.annotation.Configuration;
 @EnableZuulProxy
 public class SampleApplication {
 
+    private final static String S3_FILE_NAME = "maintenance-mode-file.json";
+    private final static String BUCKET_NAME = "bucket";
+
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
 
+//    @Bean
+//    public InMemoryMaintenanceStatusService service() {
+//        return new InMemoryMaintenanceStatusService();
+//    }
+
     @Bean
-    public InMemoryMaintenanceService inMemoryMaintenanceService() {
-        return new InMemoryMaintenanceService();
+    public S3MaintenanceStatusService service() {
+        return S3MaintenanceStatusService.builder()
+                .withAmazonS3(
+                        AmazonS3ClientBuilder.standard()
+                                .build()
+                )
+                .withBucketName(BUCKET_NAME)
+                .withFileName(S3_FILE_NAME)
+                .build();
     }
 
     @Bean
     public ZuulFilter maintenanceFilter() {
-        return new MaintenanceZuulFilter(inMemoryMaintenanceService());
+        return new MaintenanceZuulFilter(service());
     }
 
 }
